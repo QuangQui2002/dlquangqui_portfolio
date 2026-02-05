f77480c32727779d313c4ea578e4ac2fb0cd1ad6
QuangQui2002 <dlquangqui@gmail.com>
2026-02-05 18:41:46 +0700

    update portfolio 3

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 const sections = document.querySelectorAll('.section');
 const navLinks = document.querySelectorAll('.top-menu nav a');
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                // Nếu muốn hiện 1 lần rồi thôi (không ẩn lại)
+                observer.unobserve(entry.target);
+                const id = entry.target.getAttribute('id');
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if (link.getAttribute('href') === `#${id}`) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+    },
+    {
+        threshold: 0.5
+    }
+);
 
-window.addEventListener('scroll', () => {
-    let current = '';
-
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - 150;
-        if (scrollY >= sectionTop) {
-            current = section.getAttribute('id');
-        }
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href') === '#' + current) {
-            link.classList.add('active');
-        }
-    });
+sections.forEach(section => observer.observe(section));
+document.querySelectorAll('.skill-circle').forEach(skill => {
+    const percent = skill.dataset.percent;
+    const circle = skill.querySelector('circle:last-child');
+    const offset = 314 - (314 * percent) / 100;
+    circle.style.strokeDashoffset = offset;
 });
+
+const revealElements = document.querySelectorAll('.reveal');
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+
+            const bars = entry.target.querySelectorAll('.progress-bar');
+            bars.forEach(bar => {
+                const percent = bar.dataset.percent;
+                bar.style.width = percent + '%';
+            });
+
+            observer2.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.3
+});
+
+revealElements.forEach(el => observer2.observe(el));
